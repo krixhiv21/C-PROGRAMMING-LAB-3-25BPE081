4a5423f99b7b52a7e5585ab11a29a2f5b3f6fd5d
krixhiv21 <valibenchaudhari0@gmail.com>
2025-10-16 12:09:53 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("First %d odd numbers are:\n", n);
+    for(i = 1; i <= n; i++) {
+        printf("%d ", 2 * i - 1);
+    }
+    return 0;
+}

94426ba6db53c2fba7ba41f379f9d1313a294f09
krixhiv21 <valibenchaudhari0@gmail.com>
2025-10-16 12:10:32 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("First %d even numbers are:\n", n);
+    for(i = 1; i <= n; i++) {
+        printf("%d ", 2 * i);
+    }
+    return 0;
+}
